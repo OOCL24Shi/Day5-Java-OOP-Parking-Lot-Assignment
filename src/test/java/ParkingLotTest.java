ab7407fee9dba9a3e650bd7caf55fa0ab796da89
VSHI <36368406@QQ.COM>
2024-11-23 18:05:12 +0800

    [ParkingLot][Story1] Feat: Case 3 Test + Implementation

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -29,4 +29,20 @@ public class ParkingLotTest {
         //Then
         assertEquals(car, fetchedCar);
     }
+    //Story 1 Case 3
+    @Test
+    void should_return_right_car_when_fetch_given_two_different_tickets(){
+        //Given
+        ParkingLot parkingLot = new ParkingLot(10);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+        //When
+        Car fetchedCar1 = parkingLot.fetch(ticket1);
+        Car fetchedCar2 = parkingLot.fetch(ticket2);
+        //Then
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
+    }
 }
